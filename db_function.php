59c70d6fc750f5e44f4fc5006608b55c87cfbacd
System Administrator <root@Akkawoots-MacBook-Pro.local>
2018-09-17 21:07:34 +0700

    add index

--- db_function.php
+++ db_function.php
@@ -1,4 +1,5 @@
 <?php require_once("db_connection.php");?>
+<?php require_once("index.html");?>
 <?php
 
 // $id="59/154";
--- get_type.php
+++ get_type.php
@@ -1,3 +1,4 @@
+<?php require_once("index.html");?>
 <?php
 
 require_once 'db_function.php';
