9d80c07873217424bafb78fb4e8d787793da700b
Thongchai <ais_arm@hotmail.com>
2018-09-18 16:15:49 +0700

    detect data

--- post_asset.php
+++ post_asset.php
@@ -5,28 +5,29 @@
     header('Access-Control-Allow-Headers: *');
  
 
-    $id="59/154";
-    $depreciate_b="1499.25";
-    $depreciate_col="500.75";
-    $sum="55553";
+    // $id="59/154";
+    // $depreciate_b="1499.25";
+    // $depreciate_col="500.75";
+    // $sum="55553";
 
     // json response array
     $response = array("error" => FALSE);
     $postdata = file_get_contents("php://input");
     $postdata = json_decode($postdata);
+    return $postdata;
 
-    //receiving the post params
+    // receiving the post params
     // $id=$_POST['id'];
     // $depreciate_b=$_POST['depreciate_b'];
     // $depreciate_col = $_POST['depreciate_col'];
     // $sum = $_POST['sum'];
     
-    // if (isset($postdata->id) && isset($postdata->depreciate_b) && isset($postdata->depreciate_col) && isset($postdata->sum)) {
+    if (isset($postdata->id) && isset($postdata->depreciate_b) && isset($postdata->depreciate_col) && isset($postdata->sum)) {
 
-        // $id=$postdata->id;
-        // $depreciate_b=$postdata->depreciate_b;
-        // $depreciate_col=$postdata->depreciate_col;
-        // $sum=$postdata->sum;
+        $id=$postdata->id;
+        $depreciate_b=$postdata->depreciate_b;
+        $depreciate_col=$postdata->depreciate_col;
+        $sum=$postdata->sum;
 
 
         $asset = insert_asset($id, $depreciate_b, $depreciate_col, $sum);
@@ -45,10 +46,10 @@
                 $response["error_msg"] = "Unknown error occurred!";
                 echo json_encode($response);
             }
-    // }else{
-    //     $response["error"] = TRUE;
-    //     $response["error_msg"] = "Required parameters missing!";
-    //     echo json_encode($response);
-    // }
+    }else{
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Required parameters missing!";
+        echo json_encode($response);
+    }
 
 ?>
